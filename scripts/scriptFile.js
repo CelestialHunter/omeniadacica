6d049bb8ffc18c697d7ffd14e6f317f9b227b403
Andrei - Petru Balea <andrei.balea@student.upt.ro>
2023-02-08 04:40:52 +0200

    dynamic nav gen, lang support, better file struct

--- scripts/scriptFile.js
+++ scripts/scriptFile.js
@@ -18,19 +18,72 @@ function prezentare() {
     window.location.href = "#prezentare";
 }
 
-function makeNav(currentPage) 
-{
-    dict = {
-        "landing": "landingBt",
-        "projects": "projectsBt",
-        "events": "eventsBt",
-        "support": "supportBt",
-        "contact": "contactBt"
-    }
-    
-    $.get("nav.html", function(data) {
+roNavBts = {
+    "indexBt": "Prezentare",
+    "projectsBt": "Proiecte",
+    "eventsBt": "Evenimente",
+    "supportBt": "Vino alături de noi!",
+    "contactBt": "Contact"
+}
+
+enNavBts = {
+    "indexBt": "Presentation",
+    "projectsBt": "Projects",
+    "eventsBt": "Events",
+    "supportBt": "Join us!",
+    "contactBt": "Contact"
+}
+
+pageNavBts = {
+    "index": "indexBt",
+    "projects": "projectsBt",
+    "events": "eventsBt",
+    "support": "supportBt",
+    "contact": "contactBt"
+}
+
+function makeNav(currentPage, lang) 
+{    
+    $.get("/components/nav.html", function(data) {
         $("#nav-placeholder").replaceWith(data);
-        $("#" + dict[currentPage]).addClass("currentPage");
+        $("#" + pageNavBts[currentPage]).addClass("currentPage");
+
+        if (lang == "ro") {
+            $("#" + "langBt").on("click", function() {
+                english(currentPage);
+            }).addClass("fi-gb");
+            for(var key in roNavBts) {
+                $("#" + key).text(roNavBts[key]);
+            }
+
+            $("#projectsDrop").children().eq(0).text("În derulare");
+            $("#projectsDrop").children().eq(1).text("Finalizate");
+
+            $("#eventsDrop").children().eq(0).text("Culturale");
+            $("#eventsDrop").children().eq(1).text("Muzicale");
+            $("#eventsDrop").children().eq(2).text("Istorice");
+        }
+        else if(lang == "en") {
+            $("#" + "langBt").on("click", function() {
+                romanian(currentPage);
+            }).addClass("fi-ro");
+            for(var key in enNavBts) {
+                $("#" + key).text(enNavBts[key]);
+            }
+
+            $("#projectsDrop").children().eq(0).text("Ongoing");
+            $("#projectsDrop").children().eq(1).text("Completed");
+
+            $("#eventsDrop").children().eq(0).text("Cultural");
+            $("#eventsDrop").children().eq(1).text("Musical");
+            $("#eventsDrop").children().eq(2).text("Historical");
+
+            for(var key in pageNavBts) {
+                $("#" + pageNavBts[key]).attr("href", "en/" + key + ".html");
+            }
+        }
+
+        
     });
 }
 
